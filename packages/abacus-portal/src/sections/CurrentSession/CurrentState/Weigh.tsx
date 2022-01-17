88076ea8c9dc99a96990964572dba1f12453fef9
0xinsanity <noahsahar@gmail.com>
2022-01-16 23:16:11 -0500

    test

--- packages/abacus-portal/src/sections/CurrentSession/CurrentState/Weigh.tsx
+++ packages/abacus-portal/src/sections/CurrentSession/CurrentState/Weigh.tsx
@@ -49,7 +49,7 @@ const Weigh: FunctionComponent = () => {
 
   const theme = useContext(ThemeContext)
   const [appraisalValue, setAppraisalValue] = useState("")
-  const [passwordValue, setPasswordValue] = useState("")
+  const [passwordValue, setPasswordValue] = useState("") 
 
   useEffect(() => {
     const encodedVals = encodeSessionData({

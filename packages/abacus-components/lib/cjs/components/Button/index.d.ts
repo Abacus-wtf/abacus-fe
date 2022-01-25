c0be69dfb23bed071ca342cda9606a20ff9a954c
lauchness <lauchlanchisholm@gmail.com>
2022-01-25 13:38:03 -0400

    Fixing bundle issue for sc in abacus-components

--- packages/abacus-components/lib/cjs/components/Button/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare const Buttons: import("styled-components").StyledComponent<any, any, object, string | number | symbol>;
-export declare const ButtonsWhite: import("styled-components").StyledComponent<any, any, object, string | number | symbol>;
-export declare const ButtonClear: import("styled-components").StyledComponent<any, any, object, string | number | symbol>;
-export default Buttons;
--- packages/abacus-components/lib/esm/components/SeachBar/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-interface SearchProps {
-    placeholder: string;
-    onEnter: (searchValue: string) => void;
-}
-declare const _default: ({ placeholder, onEnter }: SearchProps) => JSX.Element;
-export default _default;

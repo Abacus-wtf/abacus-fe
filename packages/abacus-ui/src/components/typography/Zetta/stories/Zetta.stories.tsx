1f9a233648118e0a19fe46f4b91aa3079bce6efd
lauchness <lauchlanchisholm@gmail.com>
2022-01-27 16:26:52 -0400

    Larger typographies added

---
+++ packages/abacus-ui/src/components/typography/Zetta/stories/Zetta.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Zetta } from "../src";
+
+export default {
+  title: "atom/Zetta",
+  component: Zetta,
+  argTypes: {},
+} as ComponentMeta<typeof Zetta>;
+
+const Template: ComponentStory<typeof Zetta> = (args) => <Zetta {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: "Zetta is one size larger than h1",
+};

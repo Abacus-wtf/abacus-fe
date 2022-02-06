e7527ffaff36cc7b8331db5b6daafd37e40298f9
0xinsanity <noahsahar@gmail.com>
2022-02-06 14:32:59 -0500

    remove import

--- packages/abacus-ui/src/components/templates/PortalNavbar/src/PortalNavbar.tsx
+++ packages/abacus-ui/src/components/templates/PortalNavbar/src/PortalNavbar.tsx
@@ -1,6 +1,6 @@
 import { Button, ButtonType } from "@atoms";
 import { Logo, ProfileInfo } from "@molecules";
-import { Kilo } from "@typography";
+import { Kilo, Tera } from "@typography";
 import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 import Dropdown from "../../../../static/dropdown.svg";
@@ -27,15 +27,35 @@ const SideContainer = styled.div`
   align-items: center;
 `;
 
+const Divider = styled.div`
+  background: rgba(0, 0, 0, 0.2);
+  width: 1px;
+  height: 38px;
+`;
+
+const ImageContainer = styled.div`
+  height: 38px;
+  justify-content: center;
+  display: flex;
+`;
+
 const PortalNavbar: FunctionComponent<PortalNavbarTypes> = ({
   balance,
   profileName,
   profileIcon,
 }) => (
   <Container>
-    <SideContainer>
+    <SideContainer style={{ gridGap: 8, alignItems: "flex-end" }}>
       <Logo />
-      <img style={{ width: 24, height: 24 }} src={Dropdown} alt="Dropdown" />
+      <Divider />
+      <Tera>Crowds</Tera>
+      <ImageContainer>
+        <img
+          style={{ width: 18, marginLeft: 5, marginTop: 5 }}
+          src={Dropdown}
+          alt="Dropdown"
+        />
+      </ImageContainer>
     </SideContainer>
     <SideContainer style={{ gridGap: 32 }}>
       <Kilo>

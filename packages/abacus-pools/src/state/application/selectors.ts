9538f5f28160242d7f245f997d3a6cab811312a5
lauchness <lauchlanchisholm@gmail.com>
2022-03-29 16:52:11 -0300

    pools: adding ethToUSD state

--- packages/abacus-pools/src/state/application/selectors.ts
+++ packages/abacus-pools/src/state/application/selectors.ts
@@ -1,4 +1,6 @@
+import { createSelector } from "@reduxjs/toolkit"
 import { AppState } from "@state/index"
+import { round2Decimals } from "@utils"
 
 export const networkSymbolSelector = (
   state: AppState
@@ -8,3 +10,12 @@ export const generalizedContractErrorMessageSelector = (
   state: AppState
 ): AppState["application"]["generalizedContract"]["errorMessage"] =>
   state.application.generalizedContract.errorMessage
+
+export const ethToUSDCalculationSelector = createSelector(
+  (state: AppState) => state.application.ethToUSD,
+  (state: AppState, eth: number) => eth,
+  (ethToUSD, eth) => {
+    const value = eth * ethToUSD
+    return round2Decimals(value)
+  }
+)

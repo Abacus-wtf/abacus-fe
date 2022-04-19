6c175432ecd2977931675848f5c24f59ddcfbf83
lauchness <lauchlanchisholm@gmail.com>
2022-04-19 15:53:34 -0300

    pools: using Subgraph Types

--- packages/abacus-pools/src/state/singlePoolData/actions.ts
+++ packages/abacus-pools/src/state/singlePoolData/actions.ts
@@ -1,10 +1,13 @@
+import { SubgraphTicket } from "@models/Subgraph"
 import { createAction } from "@reduxjs/toolkit"
 import { Pool } from "@state/poolData/reducer"
-import { Bribe, Ticket, TraderProfile } from "./reducer"
+import { Bribe, TraderProfile } from "./reducer"
 
 export const getPoolData = createAction<Pool>("singlePoolData/getPoolData")
 export const getTraderProfile = createAction<TraderProfile>(
   "singlePoolData/getTraderProfile"
 )
-export const getTickets = createAction<Ticket[]>("singlePoolData/getTickets")
+export const getTickets = createAction<SubgraphTicket[]>(
+  "singlePoolData/getTickets"
+)
 export const getBribe = createAction<Bribe>("singlePoolData/getBribe")

0901fe0fa5c9132e7e240a81abbf7b36739358ef
lauchness <lauchlanchisholm@gmail.com>
2022-04-24 21:21:29 -0300

    homepage: favicon issues

--- packages/abacus-homepage/gatsby-config.js
+++ packages/abacus-homepage/gatsby-config.js
@@ -4,6 +4,7 @@ require("dotenv").config({
 })
 
 const path = require(`path`)
+const website = require("./website")
 const config = require("./website")
 
 module.exports = {
@@ -27,7 +28,16 @@ module.exports = {
       fbAppID: "",
     },
   },
+  pathPrefix: "__PATH_PREFIX__",
   plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
@@ -42,18 +52,29 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: path.join(__dirname, `src`, `images`),
-      },
-    },
+    `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Abacus Protocol Portal`,
+        display: `minimal-ui`,
+        path: `${__dirname}/src/images`,
+        icon: `src/images/logo.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: website.themeColor,
+        // Disable the loading spinner.
+        showSpinner: true,
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }

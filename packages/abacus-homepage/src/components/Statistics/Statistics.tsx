baf9e8b113be5c6b13866dc72bdb58d7c4b0c694
lauchness <lauchlanchisholm@gmail.com>
2022-04-26 17:14:05 -0300

    homepage: statistics fetch aggregate from subgraph

--- packages/abacus-homepage/src/components/Statistics/Statistics.tsx
+++ packages/abacus-homepage/src/components/Statistics/Statistics.tsx
@@ -2,6 +2,10 @@ import { Section } from "@components/Section"
 import { Media, StatInfo } from "abacus-ui"
 import React, { FunctionComponent } from "react"
 import styled from "styled-components"
+import { useQuery } from "urql"
+import { GetAggregatesDocument } from "abacus-graph"
+import { formatEther } from "ethers/lib/utils"
+import { BigNumber } from "ethers"
 
 const Container = styled(Section)`
   background: ${({ theme }) => theme.colors.utility.white};
@@ -28,12 +32,29 @@ const StyledStatInfo = styled(StatInfo)`
   `}
 `
 
-const Statistics: FunctionComponent = () => (
-  <Container>
-    <StyledStatInfo showEthIcon stat="1200" title="EVL" />
-    <StyledStatInfo stat="400" title="NFT Pools" />
-    <StyledStatInfo showEthIcon stat="2.5" title="Rewards" />
-  </Container>
-)
+const Statistics: FunctionComponent = () => {
+  const [result] = useQuery({
+    query: GetAggregatesDocument,
+  })
+  const { data } = result
+  const TVL =
+    data?.aggregate.TVL &&
+    formatEther(
+      BigNumber.from(data.aggregate.TVL).div(BigNumber.from("1000")) ?? ""
+    )
+  return (
+    <Container>
+      <StyledStatInfo showEthIcon stat={TVL} title="TVL" />
+      <StyledStatInfo
+        stat={String(data?.aggregate.totalPools ?? "-")}
+        title="NFT Pools"
+      />
+      <StyledStatInfo
+        stat={String(data?.aggregate.totalParticipants ?? "-")}
+        title="Participants"
+      />
+    </Container>
+  )
+}
 
 export { Statistics }

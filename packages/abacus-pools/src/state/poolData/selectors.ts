c5958766be1737268a24b2cf4a7fd3a369838cbd
lauchness <lauchlanchisholm@gmail.com>
2022-05-24 15:34:50 -0300

    pools: tokenLockHistorySelector ts fix

--- packages/abacus-pools/src/state/poolData/selectors.ts
+++ packages/abacus-pools/src/state/poolData/selectors.ts
@@ -5,8 +5,11 @@ import { AppState } from ".."
 export const poolSelector = createSelector(
   (state: AppState) => state.poolData.pools,
   (state: AppState, vaultId: string) => vaultId,
-  (pools: AppState["poolData"]["pools"], vaultId: string) =>
-    pools?.find((pool) => pool.vaultAddress === vaultId) ?? []
+  (pools: AppState["poolData"]["pools"], vaultId: string) => {
+    // eslint-disable-next-line no-underscore-dangle
+    const _pools = pools ?? []
+    return _pools.find((pool) => pool.vaultAddress === vaultId)
+  }
 )
 
 export const tokenLockHistorySelector = createSelector(poolSelector, (pool) =>

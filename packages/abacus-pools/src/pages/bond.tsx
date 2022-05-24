5937559d63bd05ac68f955fe8d24c1359b9c9c97
lauchness <lauchlanchisholm@gmail.com>
2022-05-24 14:24:31 -0300

    graph: making better use of Fragments

--- packages/abacus-pools/src/pages/bond.tsx
+++
@@ -1,9 +0,0 @@
-import GlobalLayout from "@layouts/index"
-import Bond from "@sections/Bond"
-import React from "react"
-
-const BondPage = (props: any) => (
-  <GlobalLayout {...props}>{/* <Bond /> */}</GlobalLayout>
-)
-
-export default BondPage

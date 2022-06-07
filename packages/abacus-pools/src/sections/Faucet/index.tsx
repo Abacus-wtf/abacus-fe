442793ae9192b0600bdf95c862e5ad445344b845
lauchness <lauchlanchisholm@gmail.com>
2022-06-07 10:24:23 -0300

    pools: mobile fixes for faucet

--- packages/abacus-pools/src/sections/Faucet/index.tsx
+++ packages/abacus-pools/src/sections/Faucet/index.tsx
@@ -1,21 +1,27 @@
 import React from "react"
-import { UniversalContainer } from "@components/global.styles"
 import Buttons from "@components/Button"
 import { useOnFaucet } from "@hooks/faucetFunc"
+import { useGetAbcBalance } from "@state/application/hooks"
+import { Container } from "../../layouts/styles"
 
 const FaucetPage: React.FC = () => {
   const { onFaucet, isPending } = useOnFaucet()
+  const getAbcBalance = useGetAbcBalance()
 
   return (
-    <UniversalContainer style={{ gridGap: 30, alignItems: "center" }}>
+    <Container>
       <Buttons
-        style={{ fontSize: 24, padding: "16px 28px" }}
+        style={{ fontSize: 24, padding: "16px 28px", margin: "0 auto" }}
         disabled={isPending}
-        onClick={() => onFaucet(() => {})}
+        onClick={() =>
+          onFaucet(() => {
+            getAbcBalance()
+          })
+        }
       >
         {isPending ? "Loading..." : "Mint ABC"}
       </Buttons>
-    </UniversalContainer>
+    </Container>
   )
 }
 

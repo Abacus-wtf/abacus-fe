bd5360a4813618d61f3e70cbb72bc25ba5df8077
lauchness <lauchlanchisholm@gmail.com>
2022-06-07 10:20:48 -0300

    pools: removing create-pool page

--- packages/abacus-pools/src/pages/create-pool.tsx
+++
@@ -1,9 +0,0 @@
-import GlobalLayout from "@layouts/index"
-// import CreatePool from "@sections/CreatePool"
-import React from "react"
-
-const CreatePoolPage = (props: any) => (
-  <GlobalLayout {...props}>{/* <CreatePool /> */}</GlobalLayout>
-)
-
-export default CreatePoolPage

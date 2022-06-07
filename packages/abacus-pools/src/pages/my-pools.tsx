54e69fc4f5436bffba79f223da761db27e17fc2b
lauchness <lauchlanchisholm@gmail.com>
2022-06-07 10:35:46 -0300

    pools: removing unused components

--- packages/abacus-pools/src/pages/my-pools.tsx
+++
@@ -1,9 +0,0 @@
-import GlobalLayout from "@layouts/index"
-// import MyPools from "@sections/MyPools"
-import React from "react"
-
-const MyPoolsPage = (props: any) => (
-  <GlobalLayout {...props}>{/* <MyPools /> */}</GlobalLayout>
-)
-
-export default MyPoolsPage

7395efaaee7d6cf244c0af646e6d03e87cf5dbb7
lauchness <lauchlanchisholm@gmail.com>
2022-07-20 16:55:54 -0300

    pools: approve function LEND on NETH

--- packages/abacus-pools/src/hooks/lendingFunc.tsx
+++ packages/abacus-pools/src/hooks/lendingFunc.tsx
@@ -3,10 +3,12 @@ import { useTransactionAdder } from "@state/transactions/hooks"
 import { parseEther } from "ethers/lib/utils"
 import { useCallback } from "react"
 import { useCurrentLendingNFT } from "@state/lending/hooks"
+import { MaxUint256 } from "@ethersproject/constants"
 import { useActiveWeb3React, useGeneralizedContractCall } from "."
 import LEND_ABI from "../config/contracts/ABC_LEND_MULTI_ABI.json"
 import ERC_721_ABI from "../config/contracts/ERC_721_ABI.json"
-import { ABC_LEND } from "../config/constants"
+import ABC_NFT_ETH_ABI from "../config/contracts/ABC_NFT_ETH_ABI.json"
+import { ABC_LEND, ABC_NFT_ETH } from "../config/constants"
 
 export const useOnAllowTransferFrom = () => {
   const { account, library } = useActiveWeb3React()
@@ -81,6 +83,41 @@ export const useOnApproveLoan = () => {
   }
 }
 
+export const useOnApproveNETH = () => {
+  const { account, library } = useActiveWeb3React()
+  const { generalizedContractCall, isPending } = useGeneralizedContractCall()
+  const addTransaction = useTransactionAdder()
+
+  const onApproveNETH = useCallback(
+    async (cb: () => void) => {
+      const nftEth = getContract(ABC_NFT_ETH, ABC_NFT_ETH_ABI, library, account)
+      const method = nftEth.approve
+      const estimate = nftEth.estimateGas.approve
+      const args = [ABC_LEND, MaxUint256]
+      const value = null
+      const txnCb = async (response: any) => {
+        addTransaction(response, {
+          summary: "Approve nETH",
+        })
+        await response.wait()
+        cb()
+      }
+      await generalizedContractCall({
+        method,
+        estimate,
+        args,
+        value,
+        cb: txnCb,
+      })
+    },
+    [library, account, generalizedContractCall, addTransaction]
+  )
+  return {
+    onApproveNETH,
+    isPending,
+  }
+}
+
 export const useOnBorrow = () => {
   const { account, library } = useActiveWeb3React()
   const { generalizedContractCall, isPending } = useGeneralizedContractCall()
@@ -90,7 +127,7 @@ export const useOnBorrow = () => {
     async (
       pool: string,
       nft: string,
-      tokenId: number,
+      tokenId: string,
       amount: string,
       cb: () => void
     ) => {
@@ -98,7 +135,7 @@ export const useOnBorrow = () => {
 
       const method = lendContract.borrow
       const estimate = lendContract.estimateGas.borrow
-      const args = [pool, nft, tokenId, parseEther(amount)]
+      const args = [pool, nft, Number(tokenId), parseEther(amount)]
       console.log(args)
       const value = null
       const txnCb = async (response: any) => {
